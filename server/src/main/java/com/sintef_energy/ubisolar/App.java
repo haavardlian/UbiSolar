8eec374726c809e2949d4d22c014784857888860
Håvard Lian <haavard@kermit.(none)>
2014-02-12 12:59:24 +0100

    Added server maven project

---
+++ server/src/main/java/com/sintef_energy/ubisolar/App.java
@@ -0,0 +1,13 @@
+package com.sintef_energy.ubisolar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
