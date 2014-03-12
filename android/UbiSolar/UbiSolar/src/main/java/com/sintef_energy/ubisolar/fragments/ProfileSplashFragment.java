c52bfdd16e23d90e8d626b3a8c7835756df2aa7e
larserikgk <larserikgk@gmail.com>
2014-03-12 13:19:46 +0100

    Removed old facebook api. Started facebook authentication

---
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/fragments/ProfileSplashFragment.java
@@ -0,0 +1,20 @@
+package com.sintef_energy.ubisolar.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.*;
+import com.sintef_energy.ubisolar.R;
+
+/**
+ * Created by Lars Erik on 12.03.14.
+ */
+public class ProfileSplashFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile_splash,
+                container, false);
+        return view;
+    }
+}
