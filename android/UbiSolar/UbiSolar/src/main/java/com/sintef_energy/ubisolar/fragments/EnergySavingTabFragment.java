ada01580cc85ceb6d5e0b5aa5949911e49ee370d
Per Øyvind Kanestrøm <perokane@gmail.com>
2014-03-25 11:15:07 +0100

    Fixed tab fragments

--- android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/fragments/EnergySavingTabFragment.java
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/fragments/EnergySavingTabFragment.java
@@ -1,16 +1,15 @@
 package com.sintef_energy.ubisolar.fragments;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.sintef_energy.ubisolar.R;
@@ -26,13 +25,11 @@ import java.util.ArrayList;
 public class EnergySavingTabFragment extends DefaultTabFragment {
 
     private static final String TAG = EnergySavingTabFragment.class.getName();
-    public static final String TAB_WORDS = "tips";
-    public static final String TAB_NUMBERS = "your";
 
     private View mRoot;
-    private TabHost mTabHost;
-    private int mCurrentTab;
     private YourAdapter yourAdapter;
+
+    private TipsPagerAdapter mPagerAdapter;
 
     public static EnergySavingTabFragment newInstance(int sectionNumber) {
         EnergySavingTabFragment fragment = new EnergySavingTabFragment();
@@ -56,12 +53,18 @@ public class EnergySavingTabFragment extends DefaultTabFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.v(TAG, "onCreateView");
         mRoot = inflater.inflate(R.layout.fragment_energy_saving_tab, container, false);
-        //mTabHost = (TabHost) mRoot.findViewById(android.R.id.tabhost);
+
         yourAdapter = new YourAdapter(getActivity(), R.layout.fragment_your_row, new ArrayList<Tip>());
+
+        if(mPagerAdapter == null)
+            mPagerAdapter = new TipsPagerAdapter(getFragmentManager(), yourAdapter);
+
         // Initialize the ViewPager and set an adapter
         ViewPager pager = (ViewPager) mRoot.findViewById(R.id.fragment_energy_saving_pager);
-        pager.setAdapter(new MyPagerAdapter(getFragmentManager(), yourAdapter));
+        pager.setAdapter(mPagerAdapter);
+
         // Bind the tabs to the ViewPager
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) mRoot.findViewById(R.id.fragment_energy_saving_tabs);
         tabs.setViewPager(pager);
@@ -81,23 +84,25 @@ public class EnergySavingTabFragment extends DefaultTabFragment {
         //updateTab(TAB_WORDS, R.id.fragment_energy_saving_tab_tips);
     }
 
-    public class MyPagerAdapter extends FragmentPagerAdapter {
+    public class TipsPagerAdapter extends FragmentStatePagerAdapter {
 
-        private final String[] TITLES = { "Tips", "Your tips"};
+        private String[] titles;
         private YourAdapter yourAdapter;
-        public MyPagerAdapter(FragmentManager fm, YourAdapter yourAdapter) {
+        public TipsPagerAdapter(FragmentManager fm, YourAdapter yourAdapter) {
             super(fm);
             this.yourAdapter = yourAdapter;
+
+            titles = getResources().getStringArray(R.array.fragment_energy_saving_tabs);
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return TITLES[position];
+            return titles[position];
         }
 
         @Override
         public int getCount() {
-            return TITLES.length;
+            return titles.length;
         }
 
         @Override
