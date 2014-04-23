8ad996c0cddbd983c1afc092ac0672129624d809
Håvard Lian <haavarhl@stud,ntnu,no>
2014-04-23 10:25:10 +0200

    Changed Compare Settings Fragment to DefaultTabFragment

--- android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/activities/DrawerActivity.java
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/activities/DrawerActivity.java
@@ -251,7 +251,7 @@ public class DrawerActivity extends FragmentActivity implements NavigationDrawer
         if(fragment != null) //todo: Add to backstack? Or add null?
             addFragment(fragment, false, true, titleNames[position]);
             //fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
-        else if(logout){
+        else if(logout) {
             onClickLogin();
         }
         else
