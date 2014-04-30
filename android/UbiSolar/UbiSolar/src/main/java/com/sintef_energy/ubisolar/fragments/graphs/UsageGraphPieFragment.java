80415e71c5e8a6d813e1a03aed6633dce6d39594
Torrib <torrib90@gmail.com>
2014-04-30 15:22:49 +0200

    Cleanup

--- android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/fragments/graphs/UsageGraphPieFragment.java
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/fragments/graphs/UsageGraphPieFragment.java
@@ -14,7 +14,6 @@ import android.widget.TextView;
 
 import com.sintef_energy.ubisolar.IView.IUsageView;
 import com.sintef_energy.ubisolar.R;
-import com.sintef_energy.ubisolar.database.energy.EnergyUsageModel;
 import com.sintef_energy.ubisolar.model.DeviceUsageList;
 import com.sintef_energy.ubisolar.utils.Resolution;
 
@@ -30,12 +29,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
-/**
- * Created by perok on 2/11/14.
- */
 public class UsageGraphPieFragment extends Fragment implements IUsageView {
 
-    private static final String ARG_SECTION_NUMBER = "section_number";
     public static final String TAG = UsageGraphLineFragment.class.getName();
 
     private View rootView;
@@ -55,7 +50,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
 
     private boolean[] mSelectedDialogItems;
 
-    private boolean mLoaded;
+    private boolean mLoaded = false;
     private int mDeviceSize;
 
 
@@ -73,10 +68,6 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
     public UsageGraphPieFragment() {
     }
 
-    /**
-     * The first call to a created fragment
-     * @param activity
-     */
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -101,14 +92,13 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
 
 
         if (mSavedState != null) {
-            mDeviceUsageList = (ArrayList<DeviceUsageList>) mSavedState.getSerializable("mDeviceUsageList");
+//            mDeviceUsageList = (ArrayList<DeviceUsageList>) mSavedState.getSerializable("mDeviceUsageList");
             mRenderer = (DefaultRenderer) mSavedState.getSerializable("mRenderer");
             mSeries = (CategorySeries) mSavedState.getSerializable("mSeries");
             mSelected = mSavedState.getInt("mSelected");
             mSelectedDialogItems = mSavedState.getBooleanArray("mSelectedDialogItems");
         }
-        else
-        {
+        else{
             setupPieGraph();
         }
 
@@ -151,8 +141,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         Log.v(TAG, " onDestroyView()");
     }
 
-    private void setupPieGraph()
-    {
+    private void setupPieGraph(){
         mRenderer.setChartTitleTextSize(20);
         mRenderer.setLabelsTextSize(15);
         mRenderer.setLegendTextSize(15);
@@ -170,8 +159,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         mRenderer.setSelectableBuffer(10);
     }
 
-    private void createPieGraph()
-    {
+    private void createPieGraph(){
         if (mChartView == null) {
             LinearLayout layout = (LinearLayout) rootView.findViewById(R.id.pieChartView);
             mChartView = ChartFactory.getPieChartView(rootView.getContext(), mSeries, mRenderer);
@@ -183,20 +171,17 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
 
                     if (seriesSelection != null) {
                         for (int i = 0; i < mSeries.getItemCount(); i++) {
-                            if(mRenderer.getSeriesRendererAt(i).isHighlighted() && i == seriesSelection.getPointIndex())
-                            {
+                            if(mRenderer.getSeriesRendererAt(i).isHighlighted() && i == seriesSelection.getPointIndex()){
                                 mRenderer.getSeriesRendererAt(i).setHighlighted(false);
                                 mSelected = -1;
                                 clearDetails();
                             }
-                            else if(i == seriesSelection.getPointIndex())
-                            {
+                            else if(i == seriesSelection.getPointIndex()){
                                 mRenderer.getSeriesRendererAt(i).setHighlighted(true);
                                 mSelected = seriesSelection.getPointIndex();
                                 updateDetails();
                             }
-                            else
-                            {
+                            else{
                                 mRenderer.getSeriesRendererAt(i).setHighlighted(false);
                             }
                         }
@@ -211,20 +196,18 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         }
     }
 
-    private void populatePieChart()
-    {
+    private void populatePieChart(){
         clearDetails();
         double totalPowerUsage = 0;
 
         for(DeviceUsageList deviceUsageList : mDeviceUsageList)
             totalPowerUsage += deviceUsageList.getTotalUsage();
 
-        for(DeviceUsageList deviceUsageList : mDeviceUsageList)
-        {
+        for(DeviceUsageList deviceUsageList : mDeviceUsageList){
             int percentage = (int) ((deviceUsageList.getTotalUsage() / totalPowerUsage) * 100);
             deviceUsageList.setPercentage(percentage);
 
-            mSeries.add(deviceUsageList.getDevice().getName() + " - " + (int) percentage + "%",
+            mSeries.add(deviceUsageList.getDevice().getName() + " - " + percentage + "%",
                     deviceUsageList.getTotalUsage());
             SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
             renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
@@ -234,8 +217,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
     }
 
     @Override
-    public void addDeviceUsage(ArrayList<DeviceUsageList> usageList)
-    {
+    public void addDeviceUsage(ArrayList<DeviceUsageList> usageList){
         mDeviceUsageList = usageList;
         setSelectedDate();
         for(DeviceUsageList u : mDeviceUsageList)
@@ -249,8 +231,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         }
     }
 
-    public void setSelectedDate()
-    {
+    public void setSelectedDate(){
         if(mSelectedDate == null) {
             if (mDeviceUsageList != null) {
                 DeviceUsageList dul = mDeviceUsageList.get(mDeviceUsageList.size() - 1);
@@ -269,8 +250,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
 
     }
 
-    private void updateDetails()
-    {
+    private void updateDetails(){
         if(mSelected > -1 && mSelected < mDeviceUsageList.size()) {
             TextView nameView = (TextView) rootView.findViewById(R.id.pieDetailsName);
             TextView descriptionView = (TextView) rootView.findViewById(R.id.pieDetailsDescription);
@@ -284,8 +264,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         }
     }
 
-    private void clearDetails()
-    {
+    private void clearDetails(){
         TextView nameView = (TextView) rootView.findViewById(R.id.pieDetailsName);
         TextView descriptionView = (TextView) rootView.findViewById(R.id.pieDetailsDescription);
         TextView powerUsageView = (TextView) rootView.findViewById(R.id.pieDetailsPowerUsage);
@@ -295,8 +274,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         powerUsageView.setText("");
     }
 
-    public void setFormat(int mode)
-    {
+    public void setFormat(int mode){
       resolution.setFormat(mode);
     }
 
@@ -320,8 +298,7 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         this.mSelectedDialogItems = mSelectedDialogItems;
     }
 
-    private String formatDate(Date date, String format)
-    {
+    private String formatDate(Date date, String format){
         SimpleDateFormat formater = new SimpleDateFormat (format);
         if(date != null)
             return formater.format(date);
@@ -329,17 +306,15 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
             return null;
     }
 
-    public void setActiveIndex(int index)
-    {
+    public void setActiveIndex(int index){
+        //TODO set selected date
     }
 
-    public int getActiveIndex()
-    {
+    public int getActiveIndex(){
         return 0;
     }
 
-    public boolean isLoaded()
-    {
+    public boolean isLoaded(){
         if(!mLoaded) {
             mLoaded = true;
             return false;
@@ -347,17 +322,10 @@ public class UsageGraphPieFragment extends Fragment implements IUsageView {
         return mLoaded;
     }
 
-    public void setDeviceSize(int size)
-    {
+    public void setDeviceSize(int size){
         mDeviceSize = size;
     }
 
-//    private void setSelectedDate()
-//    {
-//        mSelectedDate = mDeviceUsageList.get(mDeviceUsageList.size() -1).:
-//    }
-
     public void createImage(){
-
     }
 }
