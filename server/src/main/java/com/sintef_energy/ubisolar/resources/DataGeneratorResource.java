f1928a6060387853834ea24ccc62b9d468bbb065
Håvard Lian <haavarhl@stud.ntnu.no>
2014-05-01 11:09:05 +0200

    Add GET

--- server/src/main/java/com/sintef_energy/ubisolar/resources/DataGeneratorResource.java
+++ server/src/main/java/com/sintef_energy/ubisolar/resources/DataGeneratorResource.java
@@ -5,10 +5,7 @@ import com.sintef_energy.ubisolar.structs.Device;
 import com.sintef_energy.ubisolar.structs.DeviceUsage;
 import com.yammer.dropwizard.jersey.params.LongParam;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -57,6 +54,7 @@ public class DataGeneratorResource {
         return usage;
     }
 
+    @GET
     @Path("generate/")
     public Response generateData(@PathParam("user") LongParam user) {
         ArrayList<DeviceUsage> usage;
