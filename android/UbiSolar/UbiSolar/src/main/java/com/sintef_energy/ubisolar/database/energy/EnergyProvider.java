92bcd0927f698efea14442d2f91021cc75b07dbe
Torrib <torrib90@gmail.com>
2014-05-02 09:14:46 +0200

    Changes to database query

--- android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/database/energy/EnergyProvider.java
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/database/energy/EnergyProvider.java
@@ -516,8 +516,8 @@ public class EnergyProvider extends ContentProvider{
                         + "Sum(" + EnergyUsageModel.EnergyUsageEntry.COLUMN_POWER + ") As `amount` "
                         + "FROM " + EnergyUsageModel.EnergyUsageEntry.TABLE_NAME + " "
                         + "WHERE " + where + " AND " + selectionAvoidDeleteBit
-                        + "GROUP BY " + time + ", "
-                            + EnergyUsageModel.EnergyUsageEntry.COLUMN_DEVICE_ID + " "
+                        + "GROUP BY " + time //+ ", "
+//                            + EnergyUsageModel.EnergyUsageEntry.COLUMN_DEVICE_ID + " "
                         + "ORDER BY `month` ASC";
     }
 }
