d2b9b86ac199f39bc67334d3300b869012f627db
larserikgk <larserikgk@gmail.com>
2014-05-04 11:46:00 +0200

    Further changes of user for facebook optimization

--- android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/model/User.java
+++ android/UbiSolar/UbiSolar/src/main/java/com/sintef_energy/ubisolar/model/User.java
@@ -7,20 +7,7 @@ public class User {
 
     private long userId;
     private String name;
-    private byte age;
-    private byte zipCode;
-    private Residence[] residences;
-    private String country;
 
-    public User (long userId, byte zipCode, byte age, String name,
-                     Residence[] residences, String country) {
-        this.userId = userId;
-        this.zipCode = zipCode;
-        this.age = age;
-        this.name = name;
-        this.residences = residences;
-        this.country = country;
-    }
 
     public User(String name) {
         this.name = name;
@@ -47,35 +34,4 @@ public class User {
         this.name = name;
     }
 
-    public byte getAge() {
-        return age;
-    }
-
-    public void setAge(byte age) {
-        this.age = age;
-    }
-
-    public byte getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode (byte zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public Residence[] getResidences() {
-        return residences;
-    }
-
-    public void setResidences(Residence[] residences) {
-        this.residences = residences;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
 }
